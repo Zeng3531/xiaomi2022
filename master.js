b06346167ee7c67286e6b05886b6d7f26d149931
Zeng3531 <10725296+zeng3531@user.noreply.gitee.com>
2022-08-18 09:32:18 +0800

    新增ismap

--- master.js
+++ master.js
@@ -11,6 +11,21 @@ drr.forEach(item => {
     console.log(item);
 });
 
+// 新增功能
+function isMap(array, fn) {
+    // 会返回一个新数组
+    var crr = []
+    for (let i = 0; i < drr.length; i++) {
+        crr.push(fn(array[i], i, array))
+    }
+    return crr
+}
+var char = isMap(drr, function () {
+    return 1
+})
+console.log(char);
+
+
 isForeach(drr, function (value, index, array) {
     console.log(value);
 })
